e556c658ea3c5df3c96b0120edf8459c6e6c9fd8
vietcuteloveCSharp <vuvietanhsp@gmail.com>
2025-07-15 02:40:52 +0700

    user and Irepo

---
+++ Domain/Interface/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+	public interface IUserRepository
+	{
+		Task<List<User>> GetAllUsersAsync();
+		Task<User?> GetUserByIdAsync(int id);
+		Task<User?> GetUserByEmailAsync(string email);
+		Task<bool> AddUserAsync(User user);
+		Task<bool> UpdateUserAsync(int id,User user);
+		Task<bool> UserExistsAsync(int id);
+		Task<bool> UserExistsByEmailAsync(string email);
+
+	}
+}
